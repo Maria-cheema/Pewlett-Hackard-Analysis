d9505cbe28f90bb3c09aad1cf38cddc8a95d3584
Maria-cheema <120526544+Maria-cheema@users.noreply.github.com>
2023-02-05 01:44:41 -0500

    Add files via upload

--- Data/schema.sql
+++ Data/schema.sql
@@ -1,9 +1,21 @@
-EATE TABLE departments (
+CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
+
+CREATE TABLE dept_emp(
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+);
+
 
 CREATE TABLE employees (
            emp_no INT NOT NULL,
@@ -22,3 +34,4 @@ dept_no VARCHAR(4) NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 SELECT * FROM departments;EFERENCES employees(emp_no)),
+SELECT * FROM dept_emp;
